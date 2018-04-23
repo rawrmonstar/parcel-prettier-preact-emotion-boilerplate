3fd130cfbe2c93beac5b04efbb5be9d99ee59c08
Tegan Churchill <churchill.tegan@gmail.com>
2018-04-23 01:06:26 -0700

    Fix formatting

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,20 +2,20 @@ import { injectGlobal } from "emotion";
 import { bodyFont } from "./font-stacks";
 
 injectGlobal`
-html {
-	box-sizing: border-box;
-  font-size: 16px;
-  line-height: 1.4;
-}
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+    line-height: 1.4;
+  }
 
-*,
-*::before,
-*::after {
-	box-sizing: inherit;
-}
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
 
-body {
-	margin: 0;
-  font-family: ${bodyFont};
-}
+  body {
+    margin: 0;
+    font-family: ${bodyFont};
+  }
 `;
